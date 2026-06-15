302100ceeecaecbb7b6d8c1cd0df9e5aa181b621
gaidardzhiev <1v4n@protonmail.com>
2026-06-15 08:02:06 +0000

    add stage1 to POSIX shell script that will compile and run all .lt files with strace

--- try.sh
+++ try.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-[ ! -f "${L}" ] && make && printf "\n\n\n" && for f in src/*.lt; do [ -f "$f" ] && echo "*** $f ***" && ./lethargon "$f" -o "${f%.lt}.out" && "${f%.lt}.out" && strace "${f%.lt}.out" && echo "*** done ***" && printf "\n\n\n" || echo "*** failed ***"; done
+[ ! -f "${L}" ] && make && printf "\n\n\n" && for f in src/*.lt; do [ -f "$f" ] && echo "*** $f ***" && ./lethargon "$f" -o "${f%.lt}.out" && "${f%.lt}.out" && strace "${f%.lt}.out" && echo "*** done ***" && printf "\n\n\n" || echo "*** failed ***"; done && printf "\n\n\n" && for f in stage1/*.lt; do [ -f "$f" ] && echo "*** $f ***" && ./lethargon "$f" -o "${f%.lt}.out" && "${f%.lt}.out" && strace "${f%.lt}.out" && echo "*** done ***" && printf "\n\n\n" || echo "*** failed ***" && printf "\n\n\n"; done
