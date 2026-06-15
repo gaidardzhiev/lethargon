c87f74e28b2f699d74c247b0b56bde6dbda4b1aa
gaidardzhiev <1v4n@protonmail.com>
2026-06-15 19:04:39 +0000

    fix idiotic logic...

--- try.sh
+++ try.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-[ ! -f lethargon ] && make && { n() { printf "\n\n\n"; }; run() { ./lethargon "$1" -o "${1%.lt}.out" && "${1%.lt}.out" && strace "${1%.lt}.out" && n; }; n && for f in src/*.lt stage1/*.lt; do [ -f "$f" ] && run "$f"; done && n; }
+[ ! -f lethargon ] && make; { n() { printf "\n\n\n"; }; run() { ./lethargon "$1" -o "${1%.lt}.out" && "${1%.lt}.out" && strace "${1%.lt}.out" && n; }; n && for f in src/*.lt stage1/*.lt; do [ -f "$f" ] && run "$f"; done && n; }
